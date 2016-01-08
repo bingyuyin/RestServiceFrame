c0cc3746b6fb8576baeb8d753f1bbef2c7ee0eb2
bingyuyin <bingyuyin.cn@gmail.com>
2016-01-08 17:32:05 +0800

    internal: implement a simple rest api

--- rest-service/src/main/java/com/suiyu/web/rest/controller/MainController.java
+++ rest-service/src/main/java/com/suiyu/web/rest/controller/MainController.java
@@ -1,7 +1,17 @@
 package com.suiyu.web.rest.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 /**
  * Created by yinbing on 1/8/2016.
  */
+@RequestMapping("/")
 public interface MainController {
+
+    @RequestMapping(value = "test", method = RequestMethod.GET)
+    @ResponseBody
+    ResponseEntity<Object> testForGet();
 }
