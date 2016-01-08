1f58d742a01d2b7bffdfd6bfd891cd3c194d430b
bingyuyin <bingyuyin.cn@gmail.com>
2016-01-08 17:09:57 +0800

    internal: init environment

---
+++ rest-service/src/main/java/com/suiyu/web/rest/controller/MainController.java
@@ -0,0 +1,7 @@
+package com.suiyu.web.rest.controller;
+
+/**
+ * Created by yinbing on 1/8/2016.
+ */
+public interface MainController {
+}
---
+++ ws-service/src/main/java/com/suiyu/web/ws/controller/MainController.java
@@ -0,0 +1,7 @@
+package com.suiyu.web.ws.controller;
+
+/**
+ * Created by yinbing on 1/8/2016.
+ */
+public interface MainController {
+}
