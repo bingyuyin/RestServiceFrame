1db54e898bd6364dee48fecdb062de9cf70509f3
bingyuyin <bingyuyin.cn@gmail.com>
2016-01-13 14:48:50 +0800

    internal: add atmosphere service

---
+++ rest-service/src/main/java/com/suiyu/web/application/AtmosphereManagedService.java
@@ -0,0 +1,32 @@
+package com.suiyu.web.application;
+
+import org.atmosphere.config.service.*;
+import org.atmosphere.cpr.AtmosphereResource;
+
+/**
+ * Created by yinbing on 1/13/2016.
+ */
+@ManagedService(path = "/*")
+public class AtmosphereManagedService {
+
+    @Get
+    public void onGet(AtmosphereResource atmosphereResource){
+
+    }
+
+    @Post
+    public void onPost(AtmosphereResource atmosphereResource){
+
+    }
+
+    @Put
+    public void onPut(AtmosphereResource atmosphereResource){
+
+    }
+
+    @Delete
+    public void onDelete(AtmosphereResource atmosphereResource){
+
+    }
+
+}
