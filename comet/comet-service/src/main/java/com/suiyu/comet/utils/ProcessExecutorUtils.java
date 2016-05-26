b9888b3168dc903df60314ac7077811bb26b7bf1
bingyuyin <bingyuyin.cn@gmail.com>
2016-05-26 14:05:00 +0800

    internal: refactor for process executor

--- comet/comet-service/src/main/java/com/suiyu/comet/utils/ProcessExecutorUtils.java
+++ comet/comet-service/src/main/java/com/suiyu/comet/utils/ProcessExecutorUtils.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.concurrent.*;
 
 /**
  * Created by BingyuYin on 2016/5/25.
@@ -13,9 +14,10 @@ public class ProcessExecutorUtils {
 
     }
     public static class ProcessOutput {
-        public int exitVal;
-        public String errorMessage;
-        public String outputMessage;
+        public int exitVal = 0;
+        public String errorMessage = null;
+        public String outputMessage = null;
+        public Process process = null;
     }
 
     private static class StreamGobbler extends Thread {
@@ -55,9 +57,7 @@ public class ProcessExecutorUtils {
         errorStreamGobbler.start();
         outputStreamGobbler.start();
 
-        if (waitTime != -1) {
-            process.wait(waitTime);
-        } else {
+        if (waitTime != 0) {
             process.waitFor();
         }
 
@@ -65,28 +65,71 @@ public class ProcessExecutorUtils {
         res.exitVal = process.exitValue();
         res.errorMessage = errorStreamGobbler.getBufferAsString();
         res.outputMessage = outputStreamGobbler.getBufferAsString();
+        res.process = process;
         return res;
     }
 
-    public static ProcessOutput execute(String command, long waitTime) {
+    private static ProcessOutput retrieveProcessOutput(Future<Object> future, long waitTime) throws InterruptedException,
+            ExecutionException,
+            TimeoutException{
+        ProcessOutput res = null;
+        if (waitTime < 0) {
+            res = (ProcessOutput)future.get();
+        } else {
+            res = (ProcessOutput)future.get(waitTime, TimeUnit.MILLISECONDS);
+        }
+        return res;
+    }
+
+    public static ProcessOutput execute(final String command, final long waitTime) {
+
+        Future<Object> future = Executors.newSingleThreadExecutor().submit(new Callable<Object>() {
+            @Override
+            public Object call() throws Exception {
+                return collectExecutorResult(Runtime.getRuntime().exec(command), waitTime);
+            }
+        });
+
+        ProcessOutput res = null;
         try {
-            return collectExecutorResult(Runtime.getRuntime().exec(command), waitTime);
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-        } catch (InterruptedException ie) {
-            ie.printStackTrace();
+            res = retrieveProcessOutput(future, waitTime);
+            return res;
+        } catch (Exception e) {
+            e.printStackTrace();
+            if (res != null && res.process != null) {
+                res.process.destroy();
+            }
+        } finally {
+            future.cancel(true);
+        }
+
+        return null;
+
+    }
+
+    public static ProcessOutput execute(final ProcessBuilder pb, final long waitTime) {
+       Future<Object> future = Executors.newSingleThreadExecutor().submit(new Callable<Object> () {
+           @Override
+           public Object call() throws Exception {
+               return collectExecutorResult(pb.start(), waitTime);
+           }
+       });
+        ProcessOutput res = null;
+        try {
+            res = retrieveProcessOutput(future, waitTime);
+            return res;
+        } catch (Exception e) {
+            e.printStackTrace();
+            if (res != null && res.process != null) {
+                res.process.destroy();
+            }
+        } finally {
+            future.cancel(true);
         }
         return null;
     }
 
-    public static ProcessOutput execute(ProcessBuilder pb, long waitTime){
-        try {
-            return collectExecutorResult(pb.start(), waitTime);
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-        } catch (InterruptedException ie) {
-            ie.printStackTrace();
-        }
-        return null;
-    }
+
+
+
 }
